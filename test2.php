045456cdcc864a40b2e13b14e94d0f38bcd00134
k19034866 <72971324+k19034866@users.noreply.github.com>
2022-03-09 22:24:21 +0530

    Create test2.php

---
+++ test2.php
@@ -0,0 +1,47 @@
+{% load static %}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Resume Maching Result</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,400&display=swap"
+		rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.min.js"></script>
+</head>
+<style type="text/css">
+	body,
+	html {
+		font-family: 'Roboto', sans-serif !important;
+	}
+
+	html {
+		scroll-behavior: smooth;
+	}
+
+
+	.resumematching-head {
+		background: #f9f9f9;
+		padding: 25px;
+		padding-top: 15px;
+	}
+
+	.total-resumevalue {
+		background: #fff;
+		border-radius: 0px;
+	}
+
+	.resumematching-head h2 {
+		color: #4D4F5C !important;
+		font-size: 17px;
+		font-weight: 500;
+		text-align: center;
+		margin-bottom: 20px;
+	}
