c0bb2f72f564f24a7ff2ece71e82f0e6cbdefff5
k19034866 <72971324+k19034866@users.noreply.github.com>
2022-03-10 21:29:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print('Hello World')
+def downloadFile(file,filename,sha):
+    if os.path.exists(os.path.join(base_url,sha)):
+        fileDirectory = os.path.join(base_url,sha,filename)
+    else:
+        os.makedirs(os.path.join(base_url,sha))
+        fileDirectory = os.path.join(base_url,sha,filename)
+    urllib.request.urlretrieve(file,fileDirectory)
+    return fileDirectory
